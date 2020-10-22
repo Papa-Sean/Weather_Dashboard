454614b4d930a24225245517012dcc31983b965d
Papa-Sean <smsugrue@gmail.com>
2020-10-22 00:37:55 -0400

    Forcast - icons - 5 day - missing UV&local storage

--- script.js
+++ script.js
@@ -3,9 +3,25 @@ $(document).ready(function(){
     $('#submit').click(function(){
 
         var city = $('#city').val();
+        
+        
+
 
         if(city != ''){
 
+            $.ajax({
+
+                url: "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&APPID=a35dfc04904724a33f92bb9a5d159a73",
+                type: "GET",
+                dataType: "jsonp",
+                success: function(data){
+                    var widget = fshow(data);
+
+                    $("#fshow").html(widget);
+                    $("#city").val('');
+                }
+            })
+            
             $.ajax({
 
                 url: "http://api.openweathermap.org/data/2.5/find?q=" + city + "&units=imperial" + "&APPID=a35dfc04904724a33f92bb9a5d159a73",
@@ -21,19 +37,27 @@ $(document).ready(function(){
 
                 }
 
-            });
+            })
+
 
         }else{
-            $("#error").html("Field cannot be empty");
+            $("#error").html('<div class="alert alert-warning alert-dismissible" role="alert">You should enter a City.<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
         }
     });
 
 });
 
 function show(data) {
-    return "<h2><Strong>City</strong>: "+data.list[0].name +"</h2>" +
-    "<h3><strong>Weather</strong>: "+ data.list[0].main.temp +"</h3>" +
-    "<h3>Humidity: "+ data.list[0].main.humidity +"</h3>" +
-    "<h3>Wind Speed: "+ data.list[0].main.wind +"</h3>" +
-    "<h3>UV Index: </h3>" ;
+    return "<h2><strong>Local Weather For</strong>:<br> " + data.list[0].name + ", " + data.list[0].sys.country + "</h2>" +
+    "<h3><strong>Weather</strong>: " + data.list[0].main.temp + " &deg;F<img src='http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + ".png'></h3>" +
+    "<h4><strong>Humidity</strong>: " + data.list[0].main.humidity + "%</h4>" +
+    "<h4><strong>Wind Speed</strong>: " + data.list[0].wind.speed + "mph</h4>" ;
+};
+
+function fshow(data) {
+    return "<h5><strong>Current</strong>:<br> <img src='http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + ".png'><br><strong>High</strong>:" + data.list[0].main.temp_max + " &deg;F<br><strong>Low</strong>:" + data.list[0].main.temp_min + " &deg;F  </h5>" +
+    "<h5><strong>2-Day</strong>:<br> <img src='http://openweathermap.org/img/wn/" + data.list[1].weather[0].icon + ".png'><br><strong>High</strong>:" + data.list[1].main.temp_max + " &deg;F<br><strong>Low</strong>:" + data.list[1].main.temp_min + " &deg;F  </h5>" +
+    "<h5><strong>3-Day</strong>:<br> <img src='http://openweathermap.org/img/wn/" + data.list[2].weather[0].icon + ".png'><br><strong>High</strong>:" + data.list[2].main.temp_max + " &deg;F<br><strong>Low</strong>:" + data.list[2].main.temp_min + " &deg;F  </h5>" +
+    "<h5><strong>4-Day</strong>:<br> <img src='http://openweathermap.org/img/wn/" + data.list[3].weather[0].icon + ".png'><br><strong>High</strong>:" + data.list[3].main.temp_max + " &deg;F<br><strong>Low</strong>:" + data.list[3].main.temp_min + " &deg;F  </h5>" +
+    "<h5><strong>5-Day</strong>:<br> <img src='http://openweathermap.org/img/wn/" + data.list[4].weather[0].icon + ".png'><br><strong>High</strong>:" + data.list[4].main.temp_max + " &deg;F<br><strong>Low</strong>:" + data.list[4].main.temp_min + " &deg;F  </h5>" ;
 };
